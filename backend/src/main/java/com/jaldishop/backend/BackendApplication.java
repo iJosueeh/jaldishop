5bad77a7f39c4e1fe6b0e98ff2e773fcea41811f
IJosueeh <royer.tanta27@gmail.com>
2026-09-06 12:48:10 -0500

    feat: dotenv dependencia integrada para leer .env

--- backend/src/main/java/com/jaldishop/backend/BackendApplication.java
+++ backend/src/main/java/com/jaldishop/backend/BackendApplication.java
@@ -1,13 +1,28 @@
 package com.jaldishop.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMalformed()
+                .load();
+
+        dotenv.entries().forEach(entry -> {
+            if (System.getenv(entry.getKey()) == null) {
+                System.setProperty(
+                        entry.getKey(),
+                        entry.getValue()
+                );
+            }
+        });
+
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
